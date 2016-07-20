b6cb63d5c67cdc264b3e8da6954063da45d2eafa
monsta <monster76hit@gmail.com>
2016-07-20 12:59:43 +0300

    labels txt's

--- data/prepare.py
+++ data/prepare.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import csv
+import json
 
 
 def dot():
@@ -12,12 +13,12 @@ def safe_mkdir(dirpath):
 	except:
 	    os.mkdir(dirpath)
 
-def main(filepath='actions.csv', train_ratio=0.9):
+def split(csvfilepath='actions.csv', train_ratio=0.9):
 	safe_mkdir('index/')
 	safe_mkdir('train/')
 	safe_mkdir('test/')
 
-	with open(filepath, 'rb') as csvfile:
+	with open(csvfilepath, 'rb') as csvfile:
 		reader = csv.reader(csvfile, delimiter=',', quotechar='\'')
 		reader = list(reader)
 		m = len(reader)
@@ -39,6 +40,45 @@ def main(filepath='actions.csv', train_ratio=0.9):
 		print
 	os.rmdir('images/')
 
+def labels(csvfilepath='actions.csv'):
+	labels = []
+	with open(csvfilepath, 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',', quotechar='\'')
+		for index, row in enumerate(reader):
+			if index == 0: continue # skip header
+			if row[1]:
+				label = row[1].replace(' ', '_')
+				if not label in labels:
+					labels.append(label)
+	labels.sort()
+	with open('labels.txt', 'w') as f:
+		for index, label in enumerate(labels):
+			f.write("{0} {1}\n".format(index, label))
+
+	train_labels = []
+	test_labels = []
+	with open(csvfilepath, 'rb') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',', quotechar='\'')
+		for index, row in enumerate(reader):
+			if index == 0: continue # skip header
+			if row[1]:
+				index = labels.index(row[1].replace(' ', '_'))
+				if os.path.isfile('train/{0}'.format(row[0])):
+					train_labels.append((row[0], index))
+				else:
+					test_labels.append((row[0], index))
+	
+	with open('train_labels.txt', 'w') as f:
+		for fname, index in train_labels:
+			f.write("{0} {1}\n".format(fname, index))
+	with open('test_labels.txt', 'w') as f:
+		for fname, index in train_labels:
+			f.write("{0} {1}\n".format(fname, index))
+	
+
+def main():
+	# split()
+	labels()
 
 if __name__ == '__main__':
 	main()
