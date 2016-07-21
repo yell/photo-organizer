d55b1c083f7aee4df7b8fce503bb9ca25f97b00a
Veronika <yurchuk.veronika@gmail.com>
2016-07-21 21:51:11 +0300

    add

--- predict.py
+++ predict.py
@@ -4,26 +4,26 @@ import sys
 import caffe
 
 #path to image to classify
-image = caffe.io.load_image('')
+image = caffe.io.load_image("/home/veronika/materials/cv/cv_organizer/photo_organizer/data/test/000144722.jpg")
 plt.imshow(image)
 
 #path to caffe
-caffe_root = ''
+#caffe_root = ''
 
 #path to model struct
-model_def = caffe_root + ''
+model_def = "/home/veronika/caffe/models/bvlc_googlenet/train_val.prototxt"
 #path to caffemodel
-model_weights = caffe_root + ''
+model_weights = "/home/veronika/materials/cv/cv_organizer/googlenet/true/bvlc_googlenet.caffemodel"
 
 caffe.set_mode_gpu()
 net = caffe.Classifier(model_def, model_weights,
-                       mean=np.load(caffe_root + 'path to mean image!!!').mean(1).mean(1), #path to mean image
+                       #mean=np.load(caffe_root + 'path to mean image!!!').mean(1).mean(1), #path to mean image
                        channel_swap=(2,1,0),
                        raw_scale=255,
                        image_dims=(256, 256))
 
 #path to labels names
-labels_file = ''
+labels_file = '/home/veronika/materials/cv/cv_organizer/photo_organizer/data/labels.txt'
 
 labels = np.loadtxt(labels_file, str, delimiter='\t')
 
