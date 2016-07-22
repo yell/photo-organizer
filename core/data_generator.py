8f3cb8070b05776c30aea24c59f0fb2300405846
monsta <monster76hit@gmail.com>
2016-07-22 14:46:15 +0300

    added abs path

--- core/data_generator.py
+++ core/data_generator.py
@@ -10,10 +10,12 @@ from extractor import probs_feats
 
 def main(imgs_path, deploy_path, weights_path, blob_name="pool5/7x7_s1"):
 	repo = Repository()
+	if imgs_path[-1] != '/': imgs_path += '/'
 	for directory, dirnames, filenames in os.walk(imgs_path):
 		for fname in filenames:
-			probs, feats = probs_feats(fname, deploy_path, weights_path, blob_name)
-			repo.store(fname, probs, feats)
+			fpath = imgs_path + fname
+			probs, feats = probs_feats(fpath, deploy_path, weights_path, blob_name)
+			repo.store(fpath, probs, feats)
 
 if __name__ == '__main__':
 	plac.call(main)
