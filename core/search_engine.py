1092bbbef44f16fab678e0b2017db6771872a7b9
anton <antonse77@gmail.com>
2016-07-22 15:01:32 +0300

    ucoogle

--- core/search_engine.py
+++ core/search_engine.py
@@ -1,19 +1,43 @@
 import numpy as np
 import os
+import shutil
 
 import processor
 import nlp
 
-class_name = ''
+def safe_mkdir(dirpath):
+	try:
+	    os.stat(dirpath)
+	except:
+	    os.mkdir(dirpath)
 
-directory = '/home/anton/prj/photo_organizer/data/test/'
-photos = os.listdir(directory)
-for i in range(len(photos)):
-    image = directory + photos[i]
-    deploy_path = '/home/anton/prj/photo_organizer/models/googlenet/deploy.prototxt'
-    weights_path = '/home/anton/prj/photo_organizer/models/googlenet/_iter_4500.caffemodel'
-    predict = processor.probs(image, deploy_path, weights_path)
-    prediction = list(zip(predict, nlp.get_labels()))
-    prediction.sort()
-    prediction_rev = prediction[::-1]
-    
+def contents_dir(dirpath):
+    files = os.listdir(dirpath)
+    if len(files) != 0:
+        shutil.rmtree(dirpath)
+
+
+
+def search_by_class(class_name, photos_path, photos_class_path, deploy_path, weights_path):
+    photos = os.listdir(photos_path)
+    class_prob = []
+    photo_name = []
+    for i in range(len(photos)):
+        image = photos_path + photos[i]
+        predict = processor.probs_feats(image, deploy_path, weights_path)[0]
+        prediction = list(zip(predict, nlp.get_labels()))
+        prediction.sort()
+        prediction_rev = prediction[::-1]
+        if prediction_rev[0][1] == class_name:
+            class_prob.append(prediction_rev[0][0])
+            photo_name.append(photos[i])
+
+    prob_name = list(zip(class_prob, photo_name))
+    if len(prob_name) > 1:
+        prob_name = prob_name.sort(key=lambda t: -t[0])
+
+    safe_mkdir(photos_class_path)
+    contents_dir(photos_class_path)
+
+    for j in range(len(prob_name)):
+        shutil.copyfile(photos_path + prob_name[j][1], photos_class_path + prob_name[j][1])
